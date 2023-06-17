a78359075003141173d15e4be43493c1e6f5ee72
Bruno Abe <abe2602@gmail.com>
2023-06-17 17:48:46 -0300

    [android/select-folder] Add permissions

--- app/src/test/java/com/mobyle/abbay/ExampleUnitTest.kt
+++ app/src/test/java/com/mobyle/abbay/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.mobyle.abbay
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
