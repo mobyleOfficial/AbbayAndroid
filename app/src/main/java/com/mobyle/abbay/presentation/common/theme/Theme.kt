153c48efe9e245af330d726a63e12f740c1a3ff1
Bruno Abe <abe2602@gmail.com>
2025-08-09 15:01:04 -0300

    Fix status bar text color

--- app/src/main/java/com/mobyle/abbay/presentation/common/theme/Theme.kt
+++ app/src/main/java/com/mobyle/abbay/presentation/common/theme/Theme.kt
@@ -68,7 +68,7 @@ fun MyApplicationTheme(
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
             window.navigationBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
         }
     }
     MaterialTheme(
