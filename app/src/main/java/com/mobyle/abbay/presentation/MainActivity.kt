51816f8059aba8b8db69aed8be13a0333e5baa16
Bruno Abe <abe2602@gmail.com>
2025-10-04 21:35:01 -0300

    Upgrade to target 35

--- app/src/main/java/com/mobyle/abbay/presentation/MainActivity.kt
+++ app/src/main/java/com/mobyle/abbay/presentation/MainActivity.kt
@@ -3,7 +3,12 @@ package com.mobyle.abbay.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,6 +16,8 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.media3.session.MediaController
 import androidx.navigation.compose.rememberNavController
 import androidx.startup.AppInitializer
@@ -38,13 +45,28 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
-
+        enableEdgeToEdge()
         // Keep splash screen visible until player is ready
         splashScreen.setKeepOnScreenCondition {
             !isPlayerReady
         }
 
         super.onCreate(savedInstanceState)
+
+//        // Enable edge-to-edge mode
+//        // This disables the default system behavior where the window content is adjusted to fit within the system bars (status and navigation bars).
+//        // By setting this to false, your app's content is allowed to draw under the system bars, creating an immersive, edge-to-edge experience.
+//        // You can then manage how your content interacts with these bars using insets and modifiers.
+//        WindowCompat.setDecorFitsSystemWindows(window, false)
+//
+//        // Adjust status and navigation bar appearance
+//        val insetsController = WindowInsetsControllerCompat(window, window.decorView)
+//        insetsController.isAppearanceLightStatusBars = true
+//        insetsController.isAppearanceLightNavigationBars = true
+//
+//        // Optional: Set specific colors for status and navigation bars
+//        window.statusBarColor = android.graphics.Color.TRANSPARENT
+//        window.navigationBarColor = android.graphics.Color.TRANSPARENT
 
         updateAppLifeStatus(true)
 
@@ -60,7 +82,9 @@ class MainActivity : ComponentActivity() {
 
             MyApplicationTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .windowInsetsPadding(WindowInsets.systemBars),
                     color = MaterialTheme.colorScheme.primary
                 ) {
                     playerState.value?.let { player ->
@@ -86,4 +110,6 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+
 }
